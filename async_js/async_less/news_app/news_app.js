1c613bc585bab0e3bfa52415a1d56344ed519ee0
batmanInTheJungle <german8895@gmail.com>
2020-05-18 16:30:18 +0300

    change tree

---
+++ async_js/async_less/news_app/news_app.js
@@ -0,0 +1,89 @@
+// Custom Http Module
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.addEventListener("load", () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener("error", () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.addEventListener("load", () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener("error", () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+// Init http module
+const http = customHttp();
+
+const newsService = (function () {
+  const apiKey = "3cc8528d0bd548e49be8074e0a72471b";
+  const apiUrl = "http://newsapi.org/v2";
+
+  return {
+    topHeadlines(country = "ua", cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,
+        cb
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+
+//  init selects
+document.addEventListener("DOMContentLoaded", function () {
+  M.AutoInit();
+  loadNews();
+});
+
+//! Load news function
+function loadNews() {
+  newsService.topHeadlines("ua", onGetResponse);
+}
+//! function on get response from server
+function onGetResponse(err, res) {
+  console.log(res);
+}
