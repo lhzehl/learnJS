80ac8d8c6927d2f11fc837bc0926e197dff91793
batmanInTheJungle <german8895@gmail.com>
2020-05-19 10:01:36 +0300

    finish news app

--- async_js/async_less/news_app/news_app.js
+++ async_js/async_less/news_app/news_app.js
@@ -1,3 +1,4 @@
+
 // Custom Http Module
 function customHttp() {
   return {
@@ -53,7 +54,7 @@ function customHttp() {
     },
   };
 }
-// Init http module
+//* Init http module
 const http = customHttp();
 
 const newsService = (function () {
@@ -61,9 +62,9 @@ const newsService = (function () {
   const apiUrl = "http://newsapi.org/v2";
 
   return {
-    topHeadlines(country = "ua", cb) {
+    topHeadlines(country,category, cb) {
       http.get(
-        `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,
+        `${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`,
         cb
       );
     },
@@ -72,8 +73,23 @@ const newsService = (function () {
     },
   };
 })();
+//* Elements
+const form = document.forms["newsControls"];
+const countrySelect = form.elements["country"];
+const categorySelect = form.elements["category"];
+const searchInput = form.elements["search"];
+const defaultImage = 'https://auto-watt.ru/design/Auto-watt_1/images/no_image.png'
+const noContent = `
+<div class="col s12">
+  <h1>No content<h1>
+</div>
+`
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loadNews();
+});
 
-//  init selects
+//*  init selects
 document.addEventListener("DOMContentLoaded", function () {
   M.AutoInit();
   loadNews();
@@ -81,9 +97,97 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //! Load news function
 function loadNews() {
-  newsService.topHeadlines("ua", onGetResponse);
+  showPreloader();
+  const country = countrySelect.value;
+  const category = categorySelect.value;
+  const searchText = searchInput.value;
+
+  if (!searchText) {
+    newsService.topHeadlines(country,category, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
 }
 //! function on get response from server
 function onGetResponse(err, res) {
-  console.log(res);
+  removePreloader();
+  if (err) {
+    showAlert(err, "error-msg");
+    return;
+  }
+  if (!res.articles.length) {
+    const newsContainer = document.querySelector(".news-container .row");
+    clearContainer(newsContainer)
+    newsContainer.insertAdjacentHTML('afterbegin', noContent)
+    return;
+  }
+  renderNews(res.articles);
+}
+ 
+
+
+
+//! function render news
+function renderNews(news) {
+  const newsContainer = document.querySelector(".news-container .row");
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+  let fragment = "";
+  news.forEach((newsItem) => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+  newsContainer.insertAdjacentHTML("afterbegin", fragment);
+}
+//* function crear container
+
+function clearContainer(container) {
+  // container.InnerHTML = "";
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+//* News Item template function
+function newsTemplate({ urlToImage, title, url, description }) {
+  //   console.log(news);
+  return `
+  <div class="col s12">
+    <div class="card">
+    <div class="card-image">
+    <img src="${urlToImage|| defaultImage}">
+    <span class="card-title">${title || ""}</span>
+    </div>
+    <div class="card-content">
+    <p>${description || ""}</p>
+    </div>
+    <div class="card-action">
+    <a href="${url}">Read more</a>
+    </div>
+    </div>
+  </div>
+  `;
+}
+
+function showAlert(msg, type = "succeess") {
+  M.toast({ html: msg, classes: type });
+}
+//* Show loader fynction
+function showPreloader() {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `  <div class="progress">
+            <div class="indeterminate"></div>
+        </div>`
+  );
+}
+//* remove loader function
+function removePreloader() {
+  const loader = document.querySelector(".progress");
+  if (loader) {
+    loader.remove();
+  }
 }
